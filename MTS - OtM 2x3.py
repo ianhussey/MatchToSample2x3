7e3ec5d95efee1470fc1fa05cf94e4f05fec3975
ianhussey <Ian@Ians-MacBook-Air.local>
2016-04-23 12:52:06 +0100

    bug fix: if max_testing > 1, testing loop must be told to finish on criterion

--- MTS - OtM 2x3.py
+++ MTS - OtM 2x3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-This experiment was created using PsychoPy2 Experiment Builder (v1.82.01), Sat Apr 23 12:12:13 2016
+This experiment was created using PsychoPy2 Experiment Builder (v1.82.01), Sat Apr 23 12:50:11 2016
 If you publish work using this script please cite the relevant PsychoPy publications
   Peirce, JW (2007) PsychoPy - Psychophysics software in Python. Journal of Neuroscience Methods, 162(1-2), 8-13.
   Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy. Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
@@ -20,7 +20,7 @@ _thisDir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(_thisDir)
 
 # Store info about the experiment session
-expName = u'MTS - OtM 2x3'  # from the Builder filename that created this script
+expName = 'MTS - OtM 2x3'  # from the Builder filename that created this script
 expInfo = {u'gender': u'', u'age': u'', u'participant': u''}
 dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
 if dlg.OK == False: core.quit()  # user pressed cancel
@@ -46,7 +46,7 @@ endExpNow = False  # flag for 'escape' or other condition => quit the exp
 
 # Setup the Window
 win = visual.Window(size=(1366, 768), fullscr=True, screen=0, allowGUI=False, allowStencil=False,
-    monitor=u'testMonitor', color=u'black', colorSpace='rgb',
+    monitor='testMonitor', color='black', colorSpace='rgb',
     blendMode='avg', useFBO=True,
     )
 # store frame rate of monitor if we can measure it successfully
@@ -1031,6 +1031,7 @@ for thisTask in task:
                 
                 if total_correct >= testing_criterion:
                     passed_testing = True
+                    testing.finished = True
                     training_and_testing.finished = True
                 
                 thisExp.addData('passed_testing', passed_testing)
